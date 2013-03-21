220e606c961dc219505f3d3864164c303fed8018
gerkey <gerkey@osrfoundation.org>
2013-03-21 23:27:35 +0000

    workspace script seems to be working

--- tools/create_drcsim_workspace.sh
+++ tools/create_drcsim_workspace.sh
@@ -18,18 +18,25 @@ if [ $# -gt 1 ]; then
 else
   INSTALL_PREFIX=/usr
 fi
-#if [ -e $DESTDIR ]; then
-#  echo "Destination directory $DESTDIR already exists.  Aborting."
-#  exit 1
-#fi
+if [ -e $DESTDIR ]; then
+  echo "Destination directory $DESTDIR already exists.  Aborting."
+  exit 1
+fi
 
-# TODO: update to point to version in repo
-ROSINSTALL_FILE_URL=$HOME/code/drc/cloudsim-client-tools/tools/drcsim.rosinstall
+ROSINSTALL_FILE_URL=https://bitbucket.org/osrf/cloudsim-client-tools/raw/default/tools/drcsim.rosinstall
 ROSDISTRO=fuerte
+MAKE_J=-j16
+
+# Install pre-reqs, which were gathered from the debian/control files in the
+# various -release repositories
+# avr-gcc avr-libc 
+sudo apt-get install -y cmake freeglut3-dev libavcodec-dev libavformat-dev libboost-dev libboost-filesystem-dev libboost-iostreams-dev libboost-program-options-dev libboost-regex-dev libboost-signals-dev libboost-system-dev libboost-thread-dev libbullet-dev libcegui-mk2-dev libcurl4-openssl-dev libfreeimage-dev libltdl-dev libogre-dev libprotobuf-dev libprotoc-dev libqt4-dev libswscale-dev libtar-dev libtbb-dev libtinyxml-dev libxml2-dev mercurial osrf-common pkg-config protobuf-compiler robot-player-dev ros-fuerte-common-msgs ros-fuerte-console-bridge ros-fuerte-geometry ros-fuerte-geometry-experimental ros-fuerte-image-common ros-fuerte-image-pipeline ros-fuerte-pr2-controllers ros-fuerte-pr2-mechanism ros-fuerte-robot-model-visualization ros-fuerte-ros ros-fuerte-ros-comm ros-fuerte-std-msgs ros-fuerte-urdfdom ros-fuerte-xacro
 
 # Check out everything
+sudo apt-get install -y python-pip
+sudo pip install rosinstall
 echo "Assembling workspace in $DESTDIR..."
-#rosinstall $DESTDIR $ROSINSTALL_FILE_URL /opt/ros/$ROSDISTRO
+rosinstall $DESTDIR $ROSINSTALL_FILE_URL /opt/ros/$ROSDISTRO
 
 export PATH=$INSTALL_PREFIX/bin:$PATH
 export LD_LIBRARY_PATH=$INSTALL_PREFIX/lib:$LD_LIBRARY_PATH
@@ -41,7 +48,7 @@ mkdir -p $DESTDIR/build/$PROJECT
 cd $DESTDIR/build/$PROJECT
 . /opt/ros/$ROSDISTRO/setup.bash
 cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX $DESTDIR/$PROJECT
-make
+make $MAKE_J
 sudo make install
 
 # Build osrf-common
@@ -51,7 +58,7 @@ cd $DESTDIR/build/$PROJECT
 . /opt/ros/$ROSDISTRO/setup.bash
 eval export ROS_PACKAGE_PATH=$DESTDIR/$PROJECT:\$ROS_PACKAGE_PATH
 cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX $DESTDIR/$PROJECT
-make
+make $MAKE_J
 sudo make install
 
 # Build sandia-hand
@@ -59,9 +66,10 @@ PROJECT=sandia-hand
 mkdir -p $DESTDIR/build/$PROJECT
 cd $DESTDIR/build/$PROJECT
 . /opt/ros/$ROSDISTRO/setup.bash
-eval export ROS_PACKAGE_PATH=$DESTDIR/$PROJECT:$INSTALL_PREFIX/share/osrf-common-*/ros:\$ROS_PACKAGE_PATH
+# TODO: get the osrf-common version from somewhere
+eval export ROS_PACKAGE_PATH=$DESTDIR/$PROJECT:$INSTALL_PREFIX/share/osrf-common-1.0/ros:\$ROS_PACKAGE_PATH
 cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX $DESTDIR/$PROJECT
-make
+make $MAKE_J
 sudo make install
 
 # Build drcsim
@@ -70,5 +78,21 @@ mkdir -p $DESTDIR/build/$PROJECT
 cd $DESTDIR/build/$PROJECT
 . /opt/ros/$ROSDISTRO/setup.bash
 cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX $DESTDIR/$PROJECT
-make
+make $MAKE_J
 sudo make install
+
+# Generate a convenience file if we installed to somewhere other than /usr
+if [ $INSTALL_PREFIX != /usr ]; then
+  cat > /tmp/drcsim-setup.sh <<DELIM
+#!/bin/bash
+export PATH=$INSTALL_PREFIX/bin:$PATH
+export LD_LIBRARY_PATH=$INSTALL_PREFIX/lib:$LD_LIBRARY_PATH
+. $INSTALL_PREFIX/share/drcsim/setup.sh
+DELIM
+  sudo mv /tmp/drcsim-setup.sh $INSTALL_PREFIX/drcsim-setup.sh
+  echo "To configure your shell:"
+  echo "  . $INSTALL_PREFIX/drcsim-setup.sh"
+else
+  echo "To configure your shell:"
+  echo "  . /usr/share/drcsim/setup.sh"
+fi
