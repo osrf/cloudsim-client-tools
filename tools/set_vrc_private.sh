d6c94c565b2e6c8a331ea708c4be29a0a6d5be3e
Carlos Agüero <caguero@osrfoundation.org>
2013-05-10 14:12:36 -0700

    Script for installing the private arenas and all the gazebo models

---
+++ tools/set_vrc_private.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Set the private environment for testing the VRC contest
+
+USAGE="Usage: set_vrc_private.sh [-v] <bitbucket_key>"
+
+if [ $# -ne 1 ]; then
+  echo $USAGE
+  exit 1
+fi
+
+# Constants
+USER=caguero
+GAZEBO_MODELS_NAME=gazebo_models
+GAZEBO_INSTALL_DIR=/home/$USER/.gazebo
+VRC_ARENAS_NAME=vrc_arenas
+VRC_ARENA_INSTALL_DIR=/home/$USER/local
+DRCSIM_SETUP=/usr/local/share/drcsim/setup.sh
+
+# arg1: Name of the repository to install
+# arg2: Destination directory
+# arg3: ssh bitbucket key
+install ()
+{
+    cd $TMP_DIR
+    echo -n "Downloading $1..."
+
+    hg clone -e "ssh -i $3" ssh://hg@bitbucket.org/osrf/$1 > /dev/null
+    echo "Done"
+    cd $1
+    mkdir build
+    cd build
+    echo -n "Installing $1..."
+    cmake .. -DCMAKE_INSTALL_PREFIX=$2 > /dev/null
+    make install > /dev/null 2>&1
+    echo "Done"
+}
+
+TMP_DIR=`mktemp -d`
+KEY=`pwd $1`/$1
+
+# gazebo_models
+install $GAZEBO_MODELS_NAME $GAZEBO_INSTALL_DIR $KEY
+
+# vrc_arenas
+install $VRC_ARENAS_NAME $VRC_ARENA_INSTALL_DIR $KEY
+
+# Add private vrc_arenas setup.sh to the drcsim setup.sh if possible
+echo -e "\nReady. Do not forget to source the new VRC Arena:"
+echo -e "\t. $VRC_ARENA_INSTALL_DIR/share/vrc_arenas/setup.sh"
+
+# Remove temp dir
+rm -rf $TMP_DIR
